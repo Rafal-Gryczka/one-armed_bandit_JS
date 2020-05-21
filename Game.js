dbb507fddfb1121877b77cb7b18958ad57cf09e5
Rafal-Gryczka <63900821+Rafal-Gryczka@users.noreply.github.com>
2020-05-21 21:51:17 +0200

    add startGame method

--- Game.js
+++ Game.js
@@ -2,7 +2,7 @@ class Game {
     constructor(start) {
         this.stats = new Statistics();
         this.wallet = new Wallet(start);
-        document.getElementById('start').addEventListener('click', this.startGame);
+        document.getElementById('start').addEventListener('click', this.startGame.bind(this));
         this.spanWallet = document.querySelector('.panel span.wallet');
         this.boards = [...document.querySelectorAll('div.color')];
         this.inputBid = document.getElementById('bid');
@@ -21,19 +21,37 @@ class Game {
 
         this.spanWallet.textContent = money;
         if (result) {
-            result = `You've won ${wonMoney}`;
-        } else if (!result && result != "") {
-            `You 've lost ${bid}`;
+            result = `You've won ${wonMoney}$.`;
+        } else if (!result && result !== "") {
+            result = `You've lost ${bid}$.`;
 
         }
         this.spanResult.textContent = result;
         this.spanGames.textContent = stats[0];
         this.spanWins.textContent = stats[1];
         this.spanLosses.textContent = stats[2];
+        this.inputBid.value = "";
     }
 
     startGame() {
+        if (this.inputBid.value < 1) return alert('Rate is to small');
+        const bid = Math.floor(this.inputBid.value);
 
+        if (!this.wallet.checkCanPlay(bid)) {
+            return alert("You don't have enough credits to play or the value is incorrect");
+
+        }
+
+        this.wallet.changeWallet(bid, '-');
+
+        this.draw = new Draw();
+        const colors = this.draw.getDrawResult();
+        const win = Result.checkWinner(colors);
+        const wonMoney = Result.moneyWinInGame(win, bid);
+        this.wallet.changeWallet(wonMoney);
+        this.stats.addGameToStatistics(win, bid);
+
+        this.render(colors, this.wallet.getWalletValue(), win, this.stats.showGameStatistics(), bid, wonMoney);
     }
 
 }
